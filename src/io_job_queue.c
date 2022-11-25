e0f2608b1a388c3f2c3e6d93f1bed58d5bf078fb
Achim Kraus <achim.kraus@cloudcoap.net>
2022-11-25 08:04:55 +0100

    Add Kconfig for io job queue.
    Signed-off-by: Achim Kraus <achim.kraus@cloudcoap.net>

--- src/io_job_queue.c
+++ src/io_job_queue.c
@@ -13,15 +13,19 @@
 
 #include <zephyr.h>
 
+#ifdef CONFIG_USE_IO_JOB_QUEUE
+
 #define IO_JOB_QUEUE_STACK_SIZE 4096
 #define IO_JOB_QUEUE_PRIORITY 5
 
 static K_THREAD_STACK_DEFINE(io_job_queue_stack, IO_JOB_QUEUE_STACK_SIZE);
 
 struct k_work_q io_job_queue;
+#endif
 
 void io_job_queue_init(void)
 {
+#ifdef CONFIG_USE_IO_JOB_QUEUE
    struct k_work_queue_config cfg = {
        .name = "io_workq",
    };
@@ -32,21 +36,34 @@ void io_job_queue_init(void)
        K_THREAD_STACK_SIZEOF(io_job_queue_stack),
        IO_JOB_QUEUE_PRIORITY,
        &cfg);
+#endif
 }
 
 int work_schedule_for_io_queue(struct k_work_delayable *dwork,
                                k_timeout_t delay)
 {
+#ifdef CONFIG_USE_IO_JOB_QUEUE
    return k_work_schedule_for_queue(&io_job_queue, dwork, delay);
+#else
+   return k_work_schedule(dwork, delay);
+#endif
 }
 
 int work_reschedule_for_io_queue(struct k_work_delayable *dwork,
                                  k_timeout_t delay)
 {
+#ifdef CONFIG_USE_IO_JOB_QUEUE
    return k_work_reschedule_for_queue(&io_job_queue, dwork, delay);
+#else
+   return k_work_reschedule(dwork, delay);
+#endif
 }
 
 int work_submit_to_io_queue(struct k_work *work)
 {
+#ifdef CONFIG_USE_IO_JOB_QUEUE
    return k_work_submit_to_queue(&io_job_queue, work);
+#else
+   return k_work_submit(work);
+#endif
 }
