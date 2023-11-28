4c707bf08ff85b91ea2a289e6b6114f62fe7d191
Achim Kraus <achim.kraus@cloudcoap.net>
2023-11-28 16:02:50 +0100

    Use macro ITERABLE_SECTION_ROM in linker-script.
    Signed-off-by: Achim Kraus <achim.kraus@cloudcoap.net>

--- src/uart_cmd.h
+++ src/uart_cmd.h
@@ -15,8 +15,7 @@
 #define UART_CMD_H_
 
 #include <stddef.h>
-#include <zephyr/sys/util_macro.h>
-#include <zephyr/toolchain/common.h>
+#include <zephyr/sys/iterable_sections.h>
 
 typedef int (*uart_cmd_handler_t)(const char *parameter);
 typedef void (*uart_help_handler_t)(void);
@@ -30,14 +29,14 @@ struct uart_cmd_entry {
    int send;
 };
 
-#define UART_CMD(command, _at, _text, _handler, _help, _send)  \
-   static STRUCT_SECTION_ITERABLE(uart_cmd_entry, cmd_##command) = { \
-       .cmd = #command,                                        \
-       .at_cmd = _at,                                          \
-       .help = _text,                                          \
-       .handler = _handler,                                    \
-       .help_handler = _help,                                  \
-       .send = _send,                                          \
+#define UART_CMD(command, _at, _text, _handler, _help, _send)              \
+   static const STRUCT_SECTION_ITERABLE(uart_cmd_entry, cmd_##command) = { \
+       .cmd = #command,                                                    \
+       .at_cmd = _at,                                                      \
+       .help = _text,                                                      \
+       .handler = _handler,                                                \
+       .help_handler = _help,                                              \
+       .send = _send,                                                      \
    }
 
 #endif /* UART_CMD_H_ */
