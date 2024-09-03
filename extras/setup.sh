721f923085270e1ae97cfe39216aabfb1650450a
Achim Kraus <achim.kraus@cloudcoap.net>
2024-09-03 14:50:23 +0200

    Update experimental nRF9161-feather board files.
    Remove CONFIG_TFM_SECURE_UART and CONFIG_TFM_LOG_LEVEL_SILENCE.
    
    Signed-off-by: Achim Kraus <achim.kraus@cloudcoap.net>

--- extras/setup.sh
+++ extras/setup.sh
@@ -3,3 +3,6 @@
 cp -r circuitdojo_feather_nrf9161 ../../zephyr/boards/arm/
 cp mcuboot/circuitdojo_feather_nrf9161.conf ../../bootloader/mcuboot/boot/zephyr/boards
 
+cp -r conexio_stratus_pro ../../zephyr/boards/arm/
+cp mcuboot/conexio_stratus_pro.conf ../../bootloader/mcuboot/boot/zephyr/boards
+
