d15d5d1c2060ce0a1532cdbbbd01a9b1f1417f69
Achim Kraus <achim.kraus@cloudcoap.net>
2025-02-11 13:55:23 +0100

    Add beehive-scale to conexio-pro-51.
    Signed-off-by: Achim Kraus <achim.kraus@cloudcoap.net>

--- src/expansion_port.c
+++ src/expansion_port.c
@@ -20,16 +20,13 @@
 
 #include "expansion_port.h"
 #include "parse.h"
+#include "power_manager.h"
 #include "sh_cmd.h"
 
-#if DT_NODE_HAS_STATUS(DT_ALIAS(exp_port_enable), okay)
+#if DT_NODE_HAS_STATUS(DT_ALIAS(exp_port_enable), okay) || DT_NODE_HAS_STATUS(DT_ALIAS(regulator_3v3), okay)
 
-#define POWER_UP_TIME_MS 20
-#define POWER_DOWN_TIME_MS 20
-
-LOG_MODULE_DECLARE(COAP_CLIENT, CONFIG_COAP_CLIENT_LOG_LEVEL);
-
-static const struct gpio_dt_spec exp_port_gpio_spec = GPIO_DT_SPEC_GET(DT_ALIAS(exp_port_enable), gpios);
+#define POWER_UP_TIME_MS 50
+#define POWER_DOWN_TIME_MS 25
 
 enum exp_port_mode {
    EXP_PORT_AUTO,
@@ -40,10 +37,18 @@ enum exp_port_mode {
 static volatile enum exp_port_mode mode = EXP_PORT_AUTO;
 static atomic_t enable_counter = ATOMIC_INIT(0);
 
+LOG_MODULE_DECLARE(COAP_CLIENT, CONFIG_COAP_CLIENT_LOG_LEVEL);
+
+#if DT_NODE_HAS_STATUS(DT_ALIAS(exp_port_enable), okay)
+
+static const struct gpio_dt_spec exp_port_gpio_spec = GPIO_DT_SPEC_GET(DT_ALIAS(exp_port_enable), gpios);
+
+#define READY_DEVICE exp_port_gpio_spec.port
+
 static int expansion_port_power_internal(bool enable)
 {
    int ret = -ENOTSUP;
-   if (device_is_ready(exp_port_gpio_spec.port)) {
+   if (device_is_ready(READY_DEVICE)) {
       if (!enable) {
          k_sleep(K_MSEC(POWER_DOWN_TIME_MS));
       }
@@ -54,6 +59,53 @@ static int expansion_port_power_internal(bool enable)
    }
    return ret;
 }
+
+static int expansion_port_power_get(void)
+{
+   return gpio_pin_get_dt(&exp_port_gpio_spec);
+}
+
+static int expansion_port_init(void)
+{
+   if (device_is_ready(READY_DEVICE)) {
+#if defined(CONFIG_ENABLE_EXPANSION_PORT)
+      gpio_pin_configure_dt(&exp_port_gpio_spec, GPIO_OUTPUT_ACTIVE);
+#else  /* defined(CONFIG_ENABLE_EXPANSION_PORT) */
+      gpio_pin_configure_dt(&exp_port_gpio_spec, GPIO_OUTPUT_INACTIVE);
+#endif /* defined(CONFIG_ENABLE_EXPANSION_PORT) */
+   }
+   return 0;
+}
+
+SYS_INIT(expansion_port_init, POST_KERNEL, CONFIG_EXPANSION_PORT_INIT_PRIORITY);
+
+#else /* DT_NODE_HAS_STATUS(DT_ALIAS(exp_port_enable), okay) */
+
+static const struct device *npm1300_buck2_dev = DEVICE_DT_GET(DT_ALIAS(regulator_3v3));
+
+#define READY_DEVICE npm1300_buck2_dev
+
+static int expansion_port_power_internal(bool enable)
+{
+   int ret = 0;
+   if (enable || power_manager_is_suspended()) {
+      if (!enable) {
+         k_sleep(K_MSEC(POWER_DOWN_TIME_MS));
+      }
+      ret = power_manager_3v3(enable);
+      if (!ret && enable) {
+         k_sleep(K_MSEC(POWER_UP_TIME_MS));
+      }
+   }
+   return 0;
+}
+
+static int expansion_port_power_get(void)
+{
+   return power_manager_is_3v3_enabled();
+}
+
+#endif /* DT_NODE_HAS_STATUS(DT_ALIAS(exp_port_enable), okay) */
 
 int expansion_port_power(bool enable)
 {
@@ -78,20 +130,6 @@ int expansion_port_power(bool enable)
    return ret;
 }
 
-static int expansion_port_init(void)
-{
-   if (device_is_ready(exp_port_gpio_spec.port)) {
-#if defined(CONFIG_ENABLE_EXPANSION_PORT)
-      gpio_pin_configure_dt(&exp_port_gpio_spec, GPIO_OUTPUT_ACTIVE);
-#else  /* defined(CONFIG_ENABLE_EXPANSION_PORT) */
-      gpio_pin_configure_dt(&exp_port_gpio_spec, GPIO_OUTPUT_INACTIVE);
-#endif /* defined(CONFIG_ENABLE_EXPANSION_PORT) */
-   }
-   return 0;
-}
-
-SYS_INIT(expansion_port_init, POST_KERNEL, CONFIG_EXPANSION_PORT_INIT_PRIORITY);
-
 #ifdef CONFIG_SH_CMD
 
 static int expansion_port_mode(const char *config)
@@ -103,10 +141,10 @@ static int expansion_port_mode(const char *config)
    memset(value, 0, sizeof(value));
    cur = parse_next_text(cur, ' ', value, sizeof(value));
    if (!value[0]) {
-      if (!device_is_ready(exp_port_gpio_spec.port)) {
+      if (!device_is_ready(READY_DEVICE)) {
          LOG_INF("Expansion port power line not available.");
       } else {
-         res = gpio_pin_get_dt(&exp_port_gpio_spec);
+         res = expansion_port_power_get();
          cur = res > 0 ? "ON" : res < 0 ? "ERR"
                                         : "OFF";
          config = "unknown";
